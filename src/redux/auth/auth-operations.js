99b885022563328b831fdc30c11f1354c39d39c0
Dina <79221383+YermakovaDina@users.noreply.github.com>
2022-02-13 19:17:57 +0200

    Update auth-operations.js

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,51 +1,50 @@
-import axios from 'axios';
+import axios from "axios";
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { toast } from 'react-toastify';
+import {toast} from "react-toastify"
 
-axios.defaults.baseURL = 'http://localhost:3000/api/users'; //ToDO
+axios.defaults.baseURL = "http://localhost:3001/api/users"; //ToDO 
+
 
 const token = {
-  set(token) {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-  },
-  unset() {
-    axios.defaults.headers.common.Authorization = '';
-  },
+    set(token) {
+        axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    },
+    unset() {
+        axios.defaults.headers.common.Authorization = '';
+    },
 };
 
-export const register = createAsyncThunk(
-  '/signup',
-  async (credentials, { rejectWithValue }) => {
+export const register = createAsyncThunk('/signup', async (credentials, {rejectWithValue}) => {
     try {
-      const { data } = await axios.post('/signup', credentials);
-      console.log(data);
+        const { data } = await axios.post('/signup', credentials);
+        console.log(data);
+               
     } catch (error) {
-      console.log(error.response);
-      return rejectWithValue(
-        toast.error(`Something went wrong! Please, try one more time`),
-      );
+        console.log(error.response);
+        return rejectWithValue(toast.error(`Something went wrong! Please, try one more time`));
     }
-    return toast.success('You have sign up successfully!');
-  },
-);
+return toast.success('You have sign up successfully!')
+    
+});
 
-export const logIn = createAsyncThunk(
-  '/login',
-  async (credentials, { rejectWithValue }) => {
+export const logIn = createAsyncThunk('/login', async( credentials, {rejectWithValue}) => {
     try {
-      const { data } = await axios.post('/login', credentials);
-
-      token.set(data.token);
-      return data;
+        const { data } = await axios.post('/login', credentials);
+        
+        token.set(data.token);
+        return data;
     } catch (error) {
-      return rejectWithValue(toast.error(`Wrong email address or password!`));
-    }
-  },
-);
+        return rejectWithValue(toast.error(`Wrong email address or password!`));     
+            }
+    
+});
 
-export const logOut = createAsyncThunk('/logout', async () => {
-  try {
-    await axios.post('/logout');
-    token.unset();
-  } catch (error) {}
+export const logOut = createAsyncThunk('/logout', async (_, { rejectWidthValue }) => {
+    try {
+        await axios.post('/logout');
+        token.unset();
+    } catch (error) {
+        rejectWidthValue(error.message);
+        
+    }
 });
